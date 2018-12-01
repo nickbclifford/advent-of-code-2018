c98545f4e504471b04ced77e34d207741f0bd9b8
Nick Clifford <nick@nickclifford.me>
2018-12-01 11:10:15 -0600

    editorconfig

--- day1/day1.cr
+++ day1/day1.cr
@@ -4,13 +4,13 @@ found_freqs = Set(Int32).new
 all_freqs = File.read_lines("input.txt").map(&.to_i)
 
 all_freqs.cycle do |input_freq|
-    new_freq = freq + input_freq
-    
-    if found_freqs.includes?(new_freq)
-        puts new_freq
-        exit
-    else
-        freq = new_freq
-        found_freqs << new_freq
-    end
+  new_freq = freq + input_freq
+  
+  if found_freqs.includes?(new_freq)
+    puts new_freq
+    exit
+  else
+    freq = new_freq
+    found_freqs << new_freq
+  end
 end
